edb4ad924b9e304acb85f77e226bb58b7b47adaf
jerrycastillo <gweb1986@gmail.com>
2018-04-21 23:00:17 -0700

    Initial commit

---
+++ Assets/Scripts/ExplosionSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionSound : MonoBehaviour {
+
+    public AudioSource conquest;
+
+    public void Playconquest()
+    {
+        conquest.Play();
+    }
+
+	void Start()
+	{
+        // conquest.Play();
+	}
+
+	void Update()
+	{
+        // conquest.Play();
+	}
+
+	private void OnTransformChildrenChanged()
+	{
+        conquest.Play();
+	}
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+    public AudioSource hit;
+
+    public void Playhit()
+    {
+        hit.Play();
+    }
+
+    void OnTriggerEnter()
+    {
+        hit.Play();
+    }
+}
